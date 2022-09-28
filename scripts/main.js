43284c4debcb831fc636038ce939506396e70a38
AntonKornev <anton.kornev@sendpulse.com>
2022-09-29 00:48:59 +0300

    Bug fix code refactor

--- scripts/main.js
+++ scripts/main.js
@@ -1,35 +1,54 @@
-var objToday = new Date(),
-  weekday = new Array(
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-  ),
-  dayOfWeek = weekday[objToday.getDay()],
-  dayOfMonth =
-    today + (objToday.getDate() < 10)
-      ? "0" + objToday.getDate()
-      : objToday.getDate(),
-  months = new Array(
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-  ),
-  curMonth = months[objToday.getMonth()],
-  curYear = objToday.getFullYear();
+let objToday = new Date();
 
-var today = dayOfWeek + ", " + curMonth + " " + dayOfMonth + ", " + curYear;
+weekday = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday'
+];
 
-document.querySelector("#date").innerHTML = today;
+months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
+
+curMonth = months[objToday.getMonth()];
+curYear = objToday.getFullYear();
+dayOfWeek = weekday[objToday.getDay()];
+dayOfMonth = (objToday.getDate() < 10)
+  ? '0' + objToday.getDate()
+  : objToday.getDate();
+
+let tomorrow = new Date();
+tomorrow.setDate(objToday.getDate() + 1);
+
+nextMonth = months[tomorrow.getMonth()];
+nextYear = tomorrow.getFullYear();
+nextDayOfWeek = weekday[tomorrow.getDay()];
+nextDayOfMonth = (tomorrow.getDate() < 10)
+  ? '0' + tomorrow.getDate()
+  : tomorrow.getDate();
+
+const today = dayOfWeek + ', ' + curMonth + ' ' + dayOfMonth + ', ' + curYear;
+let nextDay = `${nextDayOfMonth} ${nextMonth}, ${nextYear}`;
+
+document.querySelector('#date').innerHTML = today;
+const nextDaySelectors = document.querySelectorAll('.next-day');
+
+for (let item in nextDaySelectors) {
+  console.log(nextDaySelectors[item]);
+  nextDaySelectors[item].innerHTML = nextDay;
+}
